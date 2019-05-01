02ef5dee20a8386dbab5e967e467658aed884027
tiger <1324373611@qq.com>
2019-05-01 10:36:34 +0800

    idea整合git测试

---
+++ src/main/java/pers/tiger/test/GitTest.java
@@ -0,0 +1,11 @@
+package pers.tiger.test;
+
+/**
+ * @author lig
+ * @date 2019-05-01
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("first commit!");
+    }
+}
